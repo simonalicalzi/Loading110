3c51e2a06b8532e7a2b88c63b719bb5f40921ece
simonalicalzi <simonalicalzi9@gmail.com>
2026-03-13 19:42:55 +0100

    test: ho aggiunto test media ponderata

--- src/logic.py
+++ src/logic.py
@@ -13,3 +13,10 @@ def media_aritmetica(esami: List[Esame]) -> float:
         return 0.0
     totale_voti = sum(esame.voto for esame in esami)
     return totale_voti / len(esami)
+
+def calcola_media_ponderata(esami: list[Esame]) -> float:
+    if not esami:
+        return 0.0
+    totale_punti = sum(e.voto * e.cfu for e in esami)
+    totale_cfu = sum(e.cfu for e in esami)
+    return totale_punti / totale_cfu
